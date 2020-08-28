55a84fffba817cd2e66e9e64087b1f4b0593172c
Natalie Hurst-Knight <nhurst-knight@hotmail.com>
2020-08-28 15:36:03 +0100

    added player icon to game

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,83 @@
+//  set-up a grid
+// add shooter
+// 
+
+
+
 function init() {
 
-  // * ELEMENTS
+  // * DOM ELEMENTS
   const grid = document.querySelector('.grid')
-  const width = 10
-  const cellCount = width * width
+
+  // * GRID 
+  const width = 15
+  const height = 10
+  const cellCount = width * height
   const cells = []
 
-  console.log('these are the cells', cells)
+  let shooterPosition = 142
+  const alienArmy = []
+  let alienArmyPosition = 0
 
 
   // * EXECUTION
+  function addShooter() {
+    cells[shooterPosition].classList.add('shooter')
+  }
 
-  function makeGrid() { 
-    for (let i = 0; i < cellCount; i++) {
-      const cell = document.createElement('div')
-      console.log(cell)
-      cell.setAttribute('id', i)
-      cells.push(cell)
-      grid.appendChild(cell)
-    }
+  function removeShooter() {
+    cells[shooterPosition].classList.remove('shooter')
+  }
+
+  function addAlienArmy() {
+    cells[shooterPosition].classList.add('alien-army')
+  }
+
+  function removeAlienArmy() {
+    cells[shooterPosition].classList.remove('alien-army')
   }
 
 
 
+  function makeGrid() { 
+    for (let i = 0; i < cellCount; i++) {
+      const cell = document.createElement('div')
+      grid.appendChild(cell)
+      cells.push(cell)
+      cell.setAttribute('id', i) 
+      cell.textContent = i //remove this later
+    }
+    addShooter()
+    addAlienArmy()
+  }
+
+  function moveShooter(event) {
+    removeShooter(shooterPosition)
+
+    const x = shooterPosition % width
+    
+    switch (event.keyCode) {
+      case 39:
+        if (x < width - 1) shooterPosition++
+        break
+      case 37: 
+        if (x > 0) shooterPosition--
+        break
+      default:
+        console.log('no more moves')
+    }
+    addShooter(shooterPosition)
+  }
+
+  function alienArmyDescends() {
+
+  }
+
 
   // * EVENTS
-
   makeGrid()
+  document.addEventListener('keydown', moveShooter)
+  addAlienArmy()
 
 
 
