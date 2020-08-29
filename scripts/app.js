ddf8d7e19940e478f97f0f0f38aff2ed1f13d2fc
Natalie Hurst-Knight <nhurst-knight@hotmail.com>
2020-08-29 14:16:10 +0100

    updated move aliens function

--- scripts/app.js
+++ scripts/app.js
@@ -5,18 +5,17 @@ function init() {
 
   // * DOM ELEMENTS
   const grid = document.querySelector('.grid')
-  const start = document.querySelector('.start')
-
+  
   // * GRID VARIABLES
   const width = 15
   const height = 10
   const cellCount = width * height
   const cells = []
-
+  
   // * GAME VARIABLES
   let shooterPosition = 144
-  const aliens = [0, 1, 2, 3, 4, 5, 10, 11, 12, 13 ,14, 15, 20, 21, 22, 23, 24, 25]
-
+  let aliens = [0, 1, 2, 3, 4, 5, 10, 11, 12, 13 ,14, 15, 20, 21, 22, 23, 24, 25, 26, 75, 63, 72]
+  
   // * EXECUTION
   function makeGrid() { 
     for (let i = 0; i < cellCount; i++) {
@@ -37,7 +36,7 @@ function init() {
     cells[shooterPosition].classList.remove('shooter')
   }
   
-
+  
   function addAliens(alien) {
     aliens.forEach((alien, i) => {
       cells[alien].classList.add('alien')
@@ -51,10 +50,10 @@ function init() {
     const y = null // you might need this for the lazer beams, or does this live in another place?
     
     switch (event.keyCode) {
-      case 39:
+      case 39: // RIGHT
         if (x < width - 1) shooterPosition++
         break
-      case 37: 
+      case 37: // LEFT
         if (x > 0) shooterPosition--
         break
       default:
@@ -62,25 +61,31 @@ function init() {
     }
     addShooter(shooterPosition)
   }
+      
+  function startGame() {
+    const startBtn = document.querySelector('.start')
+    // define the 'axis' that they are moving across
+    const skyline = aliens % width
+        
+    // write a for loop to run through the array of aliens
+    aliens.forEach((alien, i) => {
+      if (skyline > width + 1) {
+        aliens = aliens++
+      } else {
+        console.log('no more moves')
+      }
+    })
+  
 
-  // // function moveAlienArmy() {
-  // //   // define the 'axis' that they are moving across
-  // //   const skyline = aliens % width
-  // //   // write a for loop to run through the array of aliens
-  // //   aliens.forEach((alien, i)) => {
-
-  // //   }
-  // //   //move aliens across 1 position on the grid
-
-  // }
+  }
 
   // * EVENTS
   makeGrid()
   addShooter()
   addAliens()
-  moveAlienArmy()
-
+  
   document.addEventListener('keydown', moveShooter)
-    
+  
+  document.addEventListener('click', startGame)
 }
 window.addEventListener('DOMContentLoaded', init)
