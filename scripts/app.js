ce897e90aef7d448ee24a965fa29c169f46e6c1f
Natalie Hurst-Knight <nhurst-knight@hotmail.com>
2020-09-01 19:12:19 +0100

    refactored aliens code array and functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,21 +1,34 @@
 function init() {
   // * DOM ELEMENTS
   const grid = document.querySelector('.grid')
-
   // * GRID VARIABLES
   const width = 10
   const height = 15
   const cellCount = width * height
   const cells = []
-  
   // * GAME VARIABLES
   let shooterPosition = 144
-  let aliens = [0, 1, 2, 3, 4, 10, 11, 12, 13, 14, 20, 21, 22, 23, 24]
-  const startBtn = document.querySelector('.start') // * move this to the top with your other variables
+  let aliens = [
+    { currentIndex: 0, isAlive: true },
+    { currentIndex: 1, isAlive: true },
+    { currentIndex: 2, isAlive: true },
+    { currentIndex: 3, isAlive: true },
+    { currentIndex: 4, isAlive: true },
+    { currentIndex: 10, isAlive: true },
+    { currentIndex: 11, isAlive: true },
+    { currentIndex: 12, isAlive: true },
+    { currentIndex: 13, isAlive: true },
+    { currentIndex: 14, isAlive: true },
+    { currentIndex: 20, isAlive: true },
+    { currentIndex: 21, isAlive: true },
+    { currentIndex: 22, isAlive: true },
+    { currentIndex: 23, isAlive: true },
+    { currentIndex: 24, isAlive: true }
+  ]
+  const startBtn = document.querySelector('.start')
   let alienTimer
   let lazerPosition
   let laserTimer
-  
   // * EXECUTION
   function makeGrid() { 
     for (let i = 0; i < cellCount; i++) {
@@ -36,7 +49,6 @@ function init() {
   function moveShooter(event) {
     removeShooter(shooterPosition)
     const x = shooterPosition % width
-
     switch (event.keyCode) {
       case 39: // RIGHT
         if (x < width - 1) shooterPosition++
@@ -51,78 +63,75 @@ function init() {
         console.log('invalid key to move shooter')
     }
     addShooter(shooterPosition)
-    console.log(shooterPosition)
   }
-
   function addAliens() {
-    aliens.forEach((alien, i) => {
-      cells[alien].classList.add('alien')
+    aliens.forEach((alien) => {
+      if (alien.isAlive)
+        cells[alien.currentIndex].classList.add('alien')
     })
   }
   function removeAliens() {
-    aliens.forEach((alien, i) => {
-      cells[alien].classList.remove('alien')
+    aliens.forEach((alien) => {
+      cells[alien.currentIndex].classList.remove('alien')
     })
   }
-
-  // when the function moveAliens is called
-  // aliens will move +1 cell 
-  // when arrays reach the border of the grid +10 cells to aliens array
-  // move aliens -1
-  // when arrays reach the border of the grid +10 cells to aliens array
-  // loop back to the start of function
-  // stop function when aliens reach cell #129 (this should run endGame())
-
   function moveAliens() {
-    let count = 0
-
-  
+    // * tracking position of final alien in array
+    const finalAlien = aliens[aliens.length - 1].currentIndex
+    // * check if final alien is at the edge of the grid, if its 9 its the end of grid
+    const x = finalAlien % width
+    // * check which row we are on so we can track if we are at the bottom of the grid later
+    const y = Math.floor(finalAlien / width)
+    // * if current position of final alien is less than 9 && y is even (moving right), keep moving
+    if (x < width - 1 && y % 2 === 0) {
+      aliens = aliens.map(alien => {
+        //* for each alien spread in current object and add 1 to the index
+        return { ...alien, currentIndex: alien.currentIndex + 1 }
+      })
+      // * switch direction to right
+    } else if (x > aliens.length - 1 && y % 2 !== 0) {
+      aliens = aliens.map(alien => {
+        return { ...alien, currentIndex: alien.currentIndex - 1 }
+      })
+      // * switch direction to left
+    } else if (x === width - 1 || x === aliens.length - 1) {
+      aliens = aliens.map(alien => {
+        return { ...alien, currentIndex: alien.currentIndex + width }
+      })
+    } 
+  }
+  function startGame() {
     alienTimer = setInterval(() => {
       removeAliens()
-      aliens = aliens.map(alien => {
-        return alien + 1
-      })
+      moveAliens()
       addAliens()
-    }, 100); 
-  } 
-
-  function startGame() {
-    moveAliens()
+    },1000)
   }
-
   function addLazer() {
     cells[lazerPosition].classList.add('laser')
   }
-
   function removeLazer() {
     cells[lazerPosition].classList.remove('laser')
   }
-  
   function shootLazer() {
-    let laserCount = 0
+    const laserCount = 0
     lazerPosition = shooterPosition
     laserTimer = setInterval(() => {
       removeLazer()
       lazerPosition = lazerPosition - 10
       addLazer()
-    }, 500);
+    }, 500)
   }
-
   //this function will need to the end game if the aliens reach the cell #129 
-  function endGame() {
-    if (aliens === ) { //
-      console.log('the game needs to end')
-    } else {
-      return
-    }
-  }
+  // function endGame() {
 
+  // }
 
   // * EVENTS
   makeGrid()
   addShooter()
   addAliens()
-  endGame()
+  // endGame()
   document.addEventListener('click', startGame)
   document.addEventListener('keydown', moveShooter)
 }
